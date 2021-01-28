b8f8e073d65c93c2a36f34a9422292bfc93324d5
Samantha Cicciari <samantha.e.locke@gmail.com>
2021-01-27 20:46:59 -0600

    adding deliverable 1 materials

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,28 @@
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Unique titles
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Retiring titles
+SELECT COUNT (ut.emp_no), ut.title 
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY count DESC;
